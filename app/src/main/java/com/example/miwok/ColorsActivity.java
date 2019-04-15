82d44448e45b0600f9dc940576b82fe9e16413b3
MadhuriChelluboina <madhuri.chelluboina@outlook.com>
2019-04-15 15:35:57 -0500

    added listview and adapter to numbersActivity

--- app/src/main/java/com/example/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/miwok/ColorsActivity.java
@@ -2,6 +2,10 @@ package com.example.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+        LinearLayout rootView  = findViewById(R.id.rootView);
+        ArrayList<String> colorsList = new ArrayList<>();
+        colorsList.add("red");
+        colorsList.add("green");
+        colorsList.add("yellow");
+        colorsList.add("blue");
+
+        for(int i=0;i<colorsList.size();i++)
+        {
+            TextView textView = new TextView(this);
+            textView.setText(colorsList.get(i));
+            rootView.addView(textView);
+        }
+
+
     }
 }
--- app/src/main/java/com/example/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/miwok/NumbersActivity.java
@@ -2,6 +2,13 @@ package com.example.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +16,31 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+//        LinearLayout mNumbers_rootView  = findViewById(R.id.numbers_rootView);
+
+        ArrayList<String> mNumbersList = new ArrayList<>();
+        mNumbersList.add("one");
+        mNumbersList.add("two");
+        mNumbersList.add("three");
+        mNumbersList.add("four");
+        mNumbersList.add("five");
+        mNumbersList.add("six");
+        mNumbersList.add("seven");
+        mNumbersList.add("eight");
+        mNumbersList.add("nine");
+        mNumbersList.add("ten");
+
+
+//        for(int index=0;index<mNumbersList.size();index++)
+//        {
+//            TextView mNumbers_textView = new TextView(this);
+//            mNumbers_textView.setText(mNumbersList.get(index));
+//            mNumbers_rootView.addView(mNumbers_textView);
+//        }
+
+        ArrayAdapter<String> numbersAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,mNumbersList);
+        ListView mNumbers_ListView = findViewById(R.id.numbers_rootView);
+        mNumbers_ListView.setAdapter(numbersAdapter);
+
     }
 }
